7f6d83cf96f442d9e94fec744b4a93e6b692f3d9
Cori Drew <cori.drew@external.iherb.com>
2022-03-08 22:12:24 -0600

    More playing; Some cleanup

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-14 h-14 rounded-full block mx-auto mb-4">
-        <a href="https://howrare.is/roguesharks/2500/" target="_blank" rel="noreferrer"><Image className="rounded-3xl" alt="Rogue Sharks #2500" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width={256} height={256} /></a></div>
+        <a href="/"><Image className="rounded-3xl" alt="Rogue Sharks #2500" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width={256} height={256} /></a></div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
