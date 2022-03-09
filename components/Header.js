97ccbdcf22c5fd798449f23af39b067d4d5db8df
Cori Drew <cori.drew@external.iherb.com>
2022-03-09 14:38:34 -0600

    Add google analytics

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,24 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import Script from 'next/script';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-14 h-14 rounded-full block mx-auto mb-4">
-        <Link href="/"><Image className="rounded-3xl" alt="Rogue Sharks #2500" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width={256} height={256} /></Link></div>
+        <Link href="/" passHref><Image className="rounded-3xl" alt="Rogue Sharks #2500" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width={256} height={256} /></Link></div>
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
+        <Link href="/" passHref>
           <a>{name}</a>
         </Link>
       </p>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KB13YN06XE"></Script>
+      <Script id="ga">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KB13YN06XE');`}
+      </Script>
     </header>
   );
 }
