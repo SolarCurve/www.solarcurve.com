bf931625a3284cb9e430ce7a2f81315d239234a2
Cori Drew <cori.drew@external.iherb.com>
2022-03-08 21:08:57 -0600

    use nextjs Image tag instead of img

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <a href="https://howrare.is/roguesharks/2500/" target="_blank" rel="noreferrer"><img className="rounded-3xl" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width="200"/></a>
+      <a href="https://howrare.is/roguesharks/2500/" target="_blank" rel="noreferrer"><Image className="rounded-3xl" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width={200}/></a>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
