a159459e7649ade681eaa4deb7c0b5b76aa77c96
Cori Drew <cori.drew@external.iherb.com>
2022-03-08 20:49:45 -0600

    Updated Logo/header/footer

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <a href="https://howrare.is/roguesharks/2500/" target="_blank"><img className="rounded-3xl" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width="200"/></a>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
