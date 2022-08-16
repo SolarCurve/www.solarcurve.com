b0aede08b78b02ccfa5af926be3ae629503f0693
SolarCurve <solarcurve@gmail.com>
2022-08-16 18:49:55 -0500

    Uploading images, linking directly to the images instead of remote.

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="w-14 h-14 rounded-full block mx-auto mb-4">
-        <Link href="/" passHref><Image className="rounded-3xl" alt="Rogue Sharks #2500" src="https://media.howrare.is/images/roguesharks/cd883e64cf9199227a7567dd17a402a5.jpg" width={256} height={256} /></Link></div>
+        <Link href="/" passHref><Image className="rounded-3xl" alt="Rogue Sharks #2500" src="/images/Sentries_SolarCurve.png" width={256} height={256} /></Link></div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/" passHref>
           <a>{name}</a>
